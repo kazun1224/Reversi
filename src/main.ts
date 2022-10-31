3729d0b8668d51bbde3c1b591cfa1ed6fa3d0f6f
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-10-31 23:41:33 +0900

    盤面を取得するAPIの作成

--- src/main.ts
+++ src/main.ts
@@ -22,8 +22,8 @@ const PORT = 3000;
 
 const app = express();
 
-app.use(morgan("dev"))
-app.use(express.static('static',{extensions: ['html']}))
+app.use(morgan("dev"));
+app.use(express.static("static", { extensions: ["html"] }));
 
 app.get("/api/hello", async (req, res) => {
   res.json({
@@ -37,56 +37,120 @@ app.get("/api/error", async (req, res) => {
 
 app.post("/api/games", async (req, res) => {
   const now = new Date();
-  const connection = await mysql.createConnection({
-    host: "localhost",
-    database: "reversi",
-    user: "reversi",
-    password: "password",
-
-  });
+  const connection = await connectMySql();
   try {
-  await connection.beginTransaction()
-  const gameInsertResult =  await connection.execute<mysql.ResultSetHeader>("insert into games (started_at) values (?)",[now])
-  const gameId = gameInsertResult[0].insertId
+    await connection.beginTransaction();
+    // games tableに保存
+    const gameInsertResult = await connection.execute<mysql.ResultSetHeader>(
+      "insert into games (started_at) values (?)",
+      [now]
+    );
+    const gameId = gameInsertResult[0].insertId;
 
-  const turnInsertResult =  await connection.execute<mysql.ResultSetHeader>("insert into turns (game_id, turn_count, next_disc, end_at) values (?,?,?,?)",[gameId, 0, DARK,now]);
-  const turnId = turnInsertResult[0].insertId
+    // turns tableに保存
+    const turnInsertResult = await connection.execute<mysql.ResultSetHeader>(
+      "insert into turns (game_id, turn_count, next_disc, end_at) values (?,?,?,?)",
+      [gameId, 0, DARK, now]
+    );
+    const turnId = turnInsertResult[0].insertId;
 
-  // マスの数を計算
-  const squareCount = INITIAL_BOARD.map((line)=> line.length).reduce((v1,v2) => v1 + v2,0);
+    // マスの数を計算
+    const squareCount = INITIAL_BOARD.map((line) => line.length).reduce(
+      (v1, v2) => v1 + v2,
+      0
+    );
 
-  const squareInsertSql = 'insert into squares (turn_id, x,y,disc) values' + Array.from(Array(squareCount)).map(() => '(?, ?, ?, ?)').join(',');
+    const squareInsertSql =
+      "insert into squares (turn_id, x,y,disc) values" +
+      Array.from(Array(squareCount))
+        .map(() => "(?, ?, ?, ?)")
+        .join(",");
 
-  const squareInsertValues:any[] = [];
+    const squareInsertValues: any[] = [];
 
-  INITIAL_BOARD.forEach((line, y) => {
-    line.forEach((disc, x) => {
-      squareInsertValues.push(turnId)
-      squareInsertValues.push(x)
-      squareInsertValues.push(y)
-      squareInsertValues.push(disc)
-    })
-  })
+    INITIAL_BOARD.forEach((line, y) => {
+      line.forEach((disc, x) => {
+        squareInsertValues.push(turnId);
+        squareInsertValues.push(x);
+        squareInsertValues.push(y);
+        squareInsertValues.push(disc);
+      });
+    });
 
-  await connection.execute(squareInsertSql,squareInsertValues);
+    await connection.execute(squareInsertSql, squareInsertValues);
 
-
-  await connection.commit()
+    await connection.commit();
   } finally {
-    await connection.end()
+    await connection.end();
   }
   res.status(201).end();
 });
 
-app.use(errorHandler)
+app.get("/api/games/latest/turns/:turnCount", async (req, res) => {
+  const turnCount = parseInt(req.params.turnCount);
+
+  const connection = await connectMySql();
+  try {
+    const gameSelectResult = await connection.execute<mysql.RowDataPacket[]>(
+      "select id, started_at from games order by id desc limit 1"
+    );
+    const game = gameSelectResult[0][0];
+
+    const turnSelectResult = await connection.execute<mysql.RowDataPacket[]>(
+      "select id, game_id, turn_count, next_disc, end_at from turns where game_id = ? and turn_count = ?",
+      [game["id"], turnCount]
+    );
+    const turn = turnSelectResult[0][0];
+
+    const squareSelectResult = await connection.execute<mysql.RowDataPacket[]>(
+      "select id, turn_id x, y, disc from squares where turn_id = ?",
+      [turn["id"]]
+    );
+
+    const squares = squareSelectResult[0];
+
+    const board = Array.from(Array(8)).map(() => Array.from(Array(8)));
+    squares.forEach((s) => {
+      board[s.y][s.x] = s.disc;
+    });
+
+    const responseBody = {
+      turnCount,
+      board,
+      nextDisc: turn["next_disc"],
+      // 決着がついてる場合game_resultsテーブルから取得する
+      winnerDisk: null,
+    };
+
+    res.json(responseBody);
+  } finally {
+    await connection.end();
+  }
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Reversi application started: http://localhost:${PORT}`);
 });
 
-function errorHandler(err: any ,_req: express.Request, res: express.Response, next: express.NextFunction) {
-  console.error('Unexpected error occurred', err)
+function errorHandler(
+  err: any,
+  _req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
+  console.error("Unexpected error occurred", err);
   res.status(500).send({
-    message:"Unexpected error occurred",
-  })
+    message: "Unexpected error occurred",
+  });
+}
+
+async function connectMySql() {
+  return await mysql.createConnection({
+    host: "localhost",
+    database: "reversi",
+    user: "reversi",
+    password: "password",
+  });
 }

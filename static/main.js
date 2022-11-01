68de9050311d3765f2bde99ef064ffa93a0ba218
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-11-01 14:46:42 +0900

    石を置くイベントの実装

--- static/main.js
+++ static/main.js
@@ -15,18 +15,18 @@ const INITIAL_BOARD = [
 
 const boardElement = document.getElementById("board");
 
-async function showBoard() {
-  const turnCount = 0;
+async function showBoard(turnCount) {
   const response = await fetch(`/api/games/latest/turns/${turnCount}`);
   const responseBody = await response.json();
   const board = responseBody.board;
+  const nextDisk = responseBody.nextDisc;
 
   while (boardElement.firstChild) {
     boardElement.removeChild(boardElement.firstChild);
   }
 
-  board.forEach((line) => {
-    line.forEach((square) => {
+  board.forEach((line, y) => {
+    line.forEach((square, x) => {
       const squareElement = document.createElement("div");
       squareElement.className = "square";
 
@@ -35,22 +35,48 @@ async function showBoard() {
         const color = square === DARK ? "dark" : "light";
         stoneElement.className = `stone ${color}`;
         squareElement.appendChild(stoneElement);
+      } else {
+        squareElement.addEventListener("click", async () => {
+          console.log("clicked");
+          const nextTurnCount = turnCount + 1;
+          await registerTurn(nextTurnCount, nextDisk, x, y);
+          await showBoard(nextTurnCount);
+        });
       }
 
       boardElement.appendChild(squareElement);
     });
   });
-};
+}
 
 async function registerGame() {
-  await fetch('/api/games',{
-    method: 'POST',
-  })
+  await fetch("/api/games", {
+    method: "POST",
+  });
+}
+
+async function registerTurn(turnCount, disc, x, y) {
+  const requestBody = {
+    turnCount,
+    move: {
+      disc,
+      x,
+      y,
+    },
+  };
+
+  await fetch("/api/games/latest/turns", {
+    method: "POSt",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestBody),
+  });
 }
 
 async function main() {
   await registerGame();
-  await showBoard();
+  await showBoard(0);
 }
 
 main();

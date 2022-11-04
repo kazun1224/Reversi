d4bfd716a0ce77205223b3ab5193879a812621e7
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-11-04 22:53:48 +0900

    データベースへアクセスするTurnRepositoryの作成

---
+++ src/domain/turnRepository.ts
@@ -0,0 +1,86 @@
+import mysql from "mysql2/promise";
+import { Turn } from "./turn";
+import { SquareGateway } from "../dataaccess/squareGateway";
+import { TurnGateway } from "../dataaccess/turnGateway";
+import { MoveGateway } from "../dataaccess/moveGateway";
+import { Move } from "./move.";
+import { toDisc } from "./disc";
+import { Point } from "./point";
+import { MoveRecord } from "../dataaccess/moveRecord";
+import { Board } from "./board";
+
+const turnGateway = new TurnGateway();
+const squareGateway = new SquareGateway();
+const moveGateway = new MoveGateway();
+
+export class TurnRepository {
+  async findForGameIdAndTurnCount(
+    connection: mysql.Connection,
+    gameId: number,
+    turnCount: number
+  ): Promise<Turn> {
+    const turnRecord = await turnGateway.findForGameIdAndTurnCount(
+      connection,
+      gameId,
+      turnCount
+    );
+
+    if (!turnRecord) {
+      throw new Error("Specified record not found");
+    }
+
+    const squareRecords = await squareGateway.findForTurnId(
+      connection,
+      turnRecord.id
+    );
+
+    const board = Array.from(Array(8)).map(() => Array.from(Array(8)));
+    squareRecords.forEach((s) => {
+      board[s.y][s.x] = s.disc;
+    });
+
+    const moveRecord = await moveGateway.findForTurnId(
+      connection,
+      turnRecord.id
+    );
+
+    let move: Move | undefined;
+    if (moveRecord) {
+      move = new Move(
+        toDisc(moveRecord.disc),
+        new Point(moveRecord.x, moveRecord.y)
+      );
+    }
+
+    return new Turn(
+      gameId,
+      turnCount,
+      toDisc(turnRecord.nextDisc),
+      move,
+      new Board(board),
+      turnRecord.endAt
+    );
+  }
+
+  async save(connection: mysql.Connection, turn: Turn) {
+    const turnRecord = await turnGateway.insert(
+      connection,
+      turn.gameId,
+      turn.turnCount,
+      turn.nextDisc,
+      turn.endAt
+    );
+
+    await squareGateway.insertAll(connection, turnRecord.id, turn.board.discs);
+
+    if (turn.move) {
+      await moveGateway.insert(
+        connection,
+        turnRecord.id,
+        turn.move.disc,
+        turn.move.point.x,
+        turn.move.point.y
+      );
+    }
+  }
+}
